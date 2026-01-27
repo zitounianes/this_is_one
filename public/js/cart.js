183298ff29c2a9d439a8ad5cf70132968e832182
zitounianes <157304141+zitounianes@users.noreply.github.com>
2026-01-28 00:19:25 +0100

    Update project

--- public/js/cart.js
+++ public/js/cart.js
@@ -147,7 +147,7 @@ function renderCart() {
             <div class="cart-item-image">
                 ${item.image 
                     ? `<img src="${item.image}" alt="${item.name}">` 
-                    : '<div class="meal-placeholder"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg></div>'}
+                    : '<div class="meal-placeholder"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 11h.01"/><path d="M11 15h.01"/><path d="M16.5 4a3 3 0 0 0-2.83 2h-3.34a3 3 0 0 0-2.83-2A3 3 0 0 0 4.5 7v.29A13 13 0 0 0 12 20a13 13 0 0 0 7.5-12.71V7a3 3 0 0 0-3-3z"/></svg></div>'}
             </div>
             <div class="cart-item-details">
                 <h4 class="cart-item-name">${item.name}</h4>
